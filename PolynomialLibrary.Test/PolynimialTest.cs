0160659a1697952a94f2921655f2767b96c5b5b6
BunjakMaxim <bunjak.maxim@gmail.com>
2015-10-22 16:59:52 +0300

    домашнее задание 3

---
+++ PolynomialLibrary.Test/PolynimialTest.cs
@@ -0,0 +1,121 @@
+﻿using System;
+using System.Diagnostics;
+using NUnit.Framework;
+
+namespace PolynomialLibrary.Test
+{
+    [TestFixture]
+    public class PolynimialTest
+    {
+        [TestCase(new int[] { 3, 2, 3 }, new int[] { 2, 3, 2 }, 1)]
+        [TestCase(new int[] { 4, 3, 2, 1}, new int[] {1, 0, 1, 1, 0, 2}, 5)]
+        [TestCase(new int[] { -1, 0, 1, 1,-5 }, new int[] { 1, 1 }, 2)]
+        [TestCase(new int[] { -1, 0, -1, -10 }, new int[] { 1, -10 }, 3)]
+        public void TestOperatorADD(int[] a, int[] b, int r)
+        {
+            Polynimial p1 = new Polynimial(a);
+            Polynimial p2 = new Polynimial(b);
+            Polynimial p3;
+
+            Debug.WriteLine(p1);
+            Debug.WriteLine(p2);
+
+            p3 = p1 + p2;
+            Debug.WriteLine(p3);
+
+            Assert.AreEqual(p3.Calculate(r), p1.Calculate(r) + p2.Calculate(r));
+        }
+
+        [TestCase(new int[] { 3, 2, 3 }, new int[] { 1, 1, 1 }, 1)]
+        [TestCase(new int[] { 4, 3, 2, 1 }, new int[] { 1, 1}, 1)]
+        [TestCase(new int[] { -1, 0, 1, 1 }, new int[] { 2, 1 }, 2)]
+        [TestCase(new int[] { 2,5,3,1}, new int[] { 1, -1 }, 2)]
+        public void TestOperatorMull(int[] a, int[] b, int r)
+        {
+            Polynimial p1 = new Polynimial(a);
+            Polynimial p2 = new Polynimial(b);
+            Polynimial p3;
+
+            Debug.WriteLine(p1);
+            Debug.WriteLine(p2);
+            
+            p3 = p1 * p2;
+            Debug.WriteLine(p3);
+
+            Assert.AreEqual(p3.Calculate(r), p1.Calculate(r) * p2.Calculate(r));
+        }
+        
+        [TestCase(new int[] { 3, 2, 3 }, 3)]
+        [TestCase(new int[] { 4, 3, 2, 1 }, 5)]
+        [TestCase(new int[] { -1, 0, 1, 1 }, 2)]
+        [TestCase(new int[] { 2, 5, 3, 1 }, 7)]
+        public void TestOperatorMullConst(int[] a, int c)
+        {
+            int r = 3;
+            Polynimial p1 = new Polynimial(a), p2, p3;
+
+            Debug.WriteLine(p1);
+            p2 = p1 * c;
+            Debug.WriteLine(p2);
+            p3 = p1 * c;
+            Debug.WriteLine(p3);
+
+            Assert.AreEqual(p1.Calculate(r)*c, p2.Calculate(r));
+            Assert.AreEqual(p3.Calculate(r), p2.Calculate(r));
+        }
+
+        [TestCase(new int[] { 3, 2, 3 }, new int[] { 2, 3, 2 }, 1)]
+        [TestCase(new int[] { 4, 3, 2, 1 }, new int[] { 0, 1, 1, 0, 2 }, 1)]
+        [TestCase(new int[] { -1, 0, 1, 1 }, new int[] { 1, 1 }, 2)]
+        [TestCase(new int[] { -1, 0, -1, -1 }, new int[] { 1, -1 }, 2)]
+        public void TestOperatorMinus(int[] a, int[] b, int r)
+        {
+            Polynimial p1 = new Polynimial(a);
+            Polynimial p2 = new Polynimial(b);
+            Polynimial p3;
+
+            Debug.WriteLine(p1);
+            Debug.WriteLine(p2);
+
+            p3 = p1 - p2;
+            Debug.WriteLine(p3);
+
+            Assert.AreEqual(p3.Calculate(r), p1.Calculate(r) - p2.Calculate(r));
+        }
+
+
+        [TestCase(new int[] { 3, 2, 3 }, new int[] { 2, 3, 2},Result = false)]
+        [TestCase(new int[] { 4, 3, 2, 1 }, new int[] { 0, 1, 1, 0, 2 }, Result = false)]
+        [TestCase(new int[] { -1, 0, 1, 1 }, new int[] { 1, 1}, Result = false)]
+        [TestCase(new int[] { 1, 1, 1 }, new int[] { 1, 1, 1}, Result = true)]
+        [TestCase(new int[] { 1, 0, 1, 1 }, new int[] { 1, 1 }, Result = false)]
+        public bool TestEquals(int[] a, int[] b)
+        {
+            Polynimial p1 = new Polynimial(a);
+            Polynimial p2 = new Polynimial(b);
+
+            Debug.WriteLine(p1);
+            Debug.WriteLine(p2);
+
+            return p1 == p2;
+        }
+
+        [TestCase(new int[] { 1, 1, 1 }, new int[] { 1, 1, 1 }, Result = true)]
+        [TestCase(new int[] { -1, 0, 1, 1 }, new int[] { 1, 1 }, Result = false)]
+        public bool TestGetHashCode(int[] a, int[] b)
+        {
+            Polynimial p1 = new Polynimial(a);
+            Polynimial p2 = new Polynimial(b);
+
+            return p1.GetHashCode() == p2.GetHashCode();
+        }
+
+        [Test]
+        [ExpectedException(typeof(NullReferenceException))]
+        public void TestConstructor()
+        {
+            int[] f = null;
+            Polynimial p = new Polynimial(f);
+        }
+    }
+}
---
+++ PolynomialLibrary/Polynimial.cs
@@ -0,0 +1,171 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PolynomialLibrary
+{
+    public class Polynimial
+    {
+        private int[] array;
+
+        public Polynimial(int[] array)
+        {
+            if (array == null)
+                throw new NullReferenceException();
+
+            DeleteNullInStart(ref array);
+            this.array = new int[array.Length];
+            Array.Copy(array, this.array, array.Length);
+        }
+
+        public int Calculate(int x)
+        {
+            int value = 0, xPow = 1;
+
+            for(int i = 0; i < array.Length; i++)
+            {
+                value += array[i] * xPow;
+                xPow *= x;
+            }
+
+            return value;
+        }
+        public override string ToString()
+        {
+            int n = array.Length - 1;
+            string s = string.Format("{0} x^{1}", array[n], n);
+
+            for (int i = n - 1; i > 0; i--)
+            {
+                if (array[i] == 0) continue;
+                if (array[i] > 0)
+                    s += "+";
+                s += string.Format("{0} x^{1}", array[i], i);
+            }
+
+            if (array[0] == 0) return s;
+            if (array[0] > 0) s += "+";
+            return s + string.Format("{0}", array[0]);
+        }
+
+        public override bool Equals(object obj)
+        {
+            if (!(obj is Parallel)) return false;
+
+            return Equals((Polynimial)obj);
+        }
+
+        public override int GetHashCode()
+        {
+            return this.ToString().GetHashCode();
+        }
+        public int this[int i]
+        {
+            get
+            {
+                return array[i];
+            }
+        }
+        public static implicit operator Polynimial (int[] a)
+        {
+            return new Polynimial(a);
+        }
+        public static implicit operator int[](Polynimial p)
+        {
+            return p.array;
+        }
+        public static Polynimial operator + (Polynimial p1, Polynimial p2)
+        {
+            int minLength = Math.Min(p1.array.Length, p2.array.Length);
+            int maxLength = Math.Max(p1.array.Length, p2.array.Length);
+            int[] coef = new int[maxLength];
+            
+            for(int i = 0; i < minLength; i++)
+                coef[i] = p1.array[i] + p2.array[i];
+
+            if(p1.array.Length == maxLength)
+            {
+                for (int i = minLength; i < maxLength; i++)
+                    coef[i] = p1.array[i];
+            }
+            else
+            {
+                for (int i = minLength; i < maxLength; i++)
+                    coef[i] = p2.array[i];
+            }
+
+            DeleteNullInStart(ref coef);
+
+            return new Polynimial(coef);
+        }
+        public static Polynimial operator - (Polynimial p1, Polynimial p2)
+        {
+            int minLength = Math.Min(p1.array.Length, p2.array.Length);
+            int maxLength = Math.Max(p1.array.Length, p2.array.Length);
+            int[] coef = new int[maxLength];
+
+            for (int i = 0; i < minLength; i++)
+                coef[i] = p1.array[i] - p2.array[i];
+
+            if (p2.array.Length == maxLength)
+                for (int i = minLength; i < maxLength; i++)
+                    coef[i] = - p2.array[i];
+            else
+                for (int i = minLength; i < maxLength; i++)
+                    coef[i] = p1.array[i];
+
+            DeleteNullInStart(ref coef);
+            
+            return new Polynimial(coef);
+        }
+        public static Polynimial operator * (Polynimial p1, Polynimial p2)
+        {
+            int[] coef = new int[p1.array.Length + p2.array.Length - 1];
+
+            for (int i = 0; i < p1.array.Length; i++)
+                for (int j = 0; j < p2.array.Length; j++)
+                    coef[i + j] += p1.array[i] * p2.array[j];
+
+            return new Polynimial(coef);
+        }
+        public static Polynimial operator * (Polynimial p1, int c)
+        {
+            int[] newArray = new int[p1.array.Length];
+            Array.Copy(p1.array, newArray, p1.array.Length);
+
+            for (int i = 0; i < p1.array.Length; i++)
+                newArray[i] *= c;
+
+            return new Polynimial(newArray);
+        }
+        public static Polynimial operator * (int c, Polynimial p1)
+        {
+            return p1 * c;
+        }
+        public static bool operator == (Polynimial p1, Polynimial p2)
+        {
+            return p1.Equals(p2);
+        }
+        public static bool operator != (Polynimial p1, Polynimial p2)
+        {
+            return !p1.Equals(p2);
+        }
+        private bool Equals(Polynimial p)
+        {
+            return array.SequenceEqual(p.array);
+        }
+        private static void DeleteNullInStart(ref int[] coef)
+        {
+            int ind = coef.Length - 1;
+            while (ind > 0 && coef[ind] == 0)
+                ind--;
+
+            if (ind != 0)
+                Array.Copy(coef, coef, ind);
+            else
+                coef = new int[1];
+        }
+    }
+}
